eb2e776263951f9b6af829f0c99750dffbb180b7
nonyx_69 <noa.guilhot.pro@gmail.com>
2025-11-27 16:42:10 +0100

    page bio

--- moi.php
+++ moi.php
@@ -35,11 +35,11 @@ include 'includes/header.php';
                     <p>Projets réalisés</p>
                 </div>
                 <div class="stat">
-                    <h2>9+</h2>
+                    <h2>9</h2>
                     <p>Technologies maîtrisées</p>
                 </div>
                 <div class="stat">
-                    <h2>2+</h2>
+                    <h2>2eme</h2>
                     <p>Années d'études</p>
                 </div>
             </div>
@@ -54,7 +54,58 @@ include 'includes/header.php';
 </section>
 
 
+<section class="about-section">
+    
+    <div class="about-container">
+        
+        <div class="stack">
+            <img src="https://picsum.photos/id/1011/300/200" class="img1">
+            <img src="https://picsum.photos/id/1015/300/200" class="img2">
+            <img src="https://picsum.photos/id/1025/300/200" class="img3">
+            <img src="https://picsum.photos/id/1035/300/200" class="img4">
+        </div>
 
+        <div class="about-text">
+            <span class="about-tag">A changer</span>
+
+            <h1 class="about-title">
+                Trouver une <span class="green"> phrase </span>d'accroche.
+            </h1>
+
+            <p>
+                Actuellement en formation de développement web, je me spécialise dans la création 
+                d'expériences numériques modernes et intuitives. Mon parcours m'a permis 
+                d'acquérir une solide maîtrise des technologies front-end et back-end.
+            </p>
+
+            <p>
+                Chaque projet est une opportunité d'apprendre, de créer et de repousser mes limites, 
+                tout en développant des solutions performantes, accessibles et esthétiques.
+            </p>
+
+            <p>
+                Mon approche combine créativité, rigueur technique et souci du détail.
+            </p>
+
+            <div class="stats">
+                <div class="stat">
+                    <h2>7+</h2>
+                    <p>Projets réalisés</p>
+                </div>
+                <div class="stat">
+                    <h2>9+</h2>
+                    <p>Technologies maîtrisées</p>
+                </div>
+                <div class="stat">
+                    <h2>2+</h2>
+                    <p>Années d'études</p>
+                </div>
+            </div>
+        </div>
+
+        
+    </div>
+</section>
 
 
 
