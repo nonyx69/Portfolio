1098a60b6bb3a04d04cf5100e828b53ec6b66327
nonyx_69 <noa.guilhot.pro@gmail.com>
2025-11-27 01:11:57 +0100

    avancement page bio

--- moi.php
+++ moi.php
@@ -4,31 +4,131 @@ $page_css = "moi.css";
 include 'includes/header.php';
 ?>
 
-<h1>Mon Parcour Scolaire</h1>
+<section class="about-section">
+    <div class="about-container">
 
+        <div class="about-text">
+            <span class="about-tag">À propos de moi</span>
 
+            <h1 class="about-title">
+                Passionné par le <span class="green">développement web</span>
+            </h1>
 
+            <p>
+                Actuellement en formation de développement web, je me spécialise dans la création 
+                d'expériences numériques modernes et intuitives. Mon parcours m'a permis 
+                d'acquérir une solide maîtrise des technologies front-end et back-end.
+            </p>
 
+            <p>
+                Chaque projet est une opportunité d'apprendre, de créer et de repousser mes limites, 
+                tout en développant des solutions performantes, accessibles et esthétiques.
+            </p>
 
-    <div class="placement">
-    <div class="moi-card">
-        <img src="moi2.png" alt="Noa">
-        <div class="slide-text">
-            <p>Présentation perso</p>
+            <p>
+                Mon approche combine créativité, rigueur technique et souci du détail.
+            </p>
+
+            <div class="stats">
+                <div class="stat">
+                    <h2>7+</h2>
+                    <p>Projets réalisés</p>
+                </div>
+                <div class="stat">
+                    <h2>9+</h2>
+                    <p>Technologies maîtrisées</p>
+                </div>
+                <div class="stat">
+                    <h2>2+</h2>
+                    <p>Années d'études</p>
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="moi-card">
-    <img src="survie.png" alt="survie">
-    <div class="slide-text">
-        <p>Creation </p>
-    </div>
-    </div>
-    <div class="moi-card">
-        <img src="japon.jpg" alt="japon">
-        <div class="slide-text">
-            <p>Hobit (voyage au Jap)</p>
+
+        <div class="about-img-container">
+            <div class="about-img-bg"></div>
+            <img src="moi2.png" class="about-img" alt="">
         </div>
+
     </div>
-</div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+<!--
+      <div class="container">
+            <div class="gray">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSmhhVShxT8jugCFZPjnRdy8OGwyWkqa9b2qMHZBB16bnWzprZNMWj-7zcG3atTyP34dpk&usqp=CAU"/>
+                <div class="contenu">
+                    <h2>Bac</h2>
+                    <p>My name is Noa Guilhot, and I'm 19 years old.</p>
+                </div>
+            </div>
+            
+            
+            <div class="gray">
+                <img src="https://s3-eu-west-1.amazonaws.com/assets.atout-on-line.com/images/bts/2025/logos/IPSSI-logo-300.jpg"/>
+                <div class="contenu">
+                    <h2>Bachelor</h2>
+                    <p>More content coming soon!</p>
+                </div>
+            </div>
+
+            <div class="gray">
+                <img src="https://s3-eu-west-1.amazonaws.com/assets.atout-on-line.com/images/bts/2025/logos/IPSSI-logo-300.jpg" alt=""/>
+                <div class="contenu">
+                    <h2>Master</h2>
+                    <p>I love traveling and playing video games.</p>
+                </div>
+            </div>    
+        </div>
+
+
+        <div class="container2">
+            <div class="box">
+                <img src="moi2.png"/>
+                <div class="contenu"><br>
+                    <h2>A propos de moi</h2>
+                    <p>My name is Guilhot Noa, I'm 19 years old.</p>
+                </div>
+            </div>
+            
+            
+            <div class="box">
+                <img src="survie.png"/>
+                <div class="contenu"><br>
+                    <h2>Jsp encore</h2>
+                    <p>jsp non plus mdrr</p>
+                </div>
+            </div>
+
+            <div class="box">
+                <img src="japon.jpg" alt=""/>
+                <div class="contenu"><br>
+                    <h2>Activité</h2>
+                    <p>I love travel and play video game</p>
+                </div>
+            </div>    
+        </div>
+
+-->
+
+
+
+
+
+
+
+
+        
+
 
 <?php include "includes/footer.php"; ?>
